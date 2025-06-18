7bd155c58a57e88351373d9f05ddc5a2d638e36e
larrivain <daphne.larrivain@ecole.ensicaen.fr>
2025-06-18 13:11:15 +0200

    match-case for search

--- src/pyparse/generic/Search.py
+++ src/pyparse/generic/Search.py
@@ -73,50 +73,42 @@ class Search:
                     self.context.update_file(fileInfo)           
                     if self.verbose:
                         print("[INFO] [Search] Parsing " + str(fileInfo.full_path))                  
-                    self._depth_first_search()
+                    self._depth_first_search(self.context.file.get_ast())
                                                                               
-    def _depth_first_search(self):
-        def _walk_search(node, parent=None):
-            if not any(ast.iter_child_nodes(node)):  
-                return
-                        
-            if isinstance(node, ast.ClassDef):
+    def _depth_first_search(self, node, parent=None):
+        if not any(ast.iter_child_nodes(node)):  
+            return
+            
+        match node:         
+            case ast.ClassDef():
                 classInfo = self.context.build_class(node)
                 self.context.update_class(classInfo)
-                self._classes[node.name] = classInfo
-                           
-            elif isinstance(node, ast.FunctionDef):
+                self._classes[node.name] = classInfo                          
+            case ast.FunctionDef():
                 funcInfo = self.context.build_func(node)
                 self.context.update_func(funcInfo)
                 if funcInfo.is_method() or funcInfo.is_static_method():
                     self._classes[self.context.cur_class_name].add_method(funcInfo)
                 else:
-                    self._funcs.append(funcInfo)
-            
-            elif isinstance(node, ast.Call):
-                self._opcalls.add(self.context.build_call(node))
-            
-            elif isinstance(node, ast.Import):
-                self._imports.update(alias.name for alias in node.names) 
-                 
-            elif isinstance(node, ast.ImportFrom):
-                self._import_froms.extend(self.context.build_import_froms(node)) 
-                
-            elif isinstance(node, ast.Assign) or isinstance(node, ast.AnnAssign) or isinstance(node, ast.AugAssign):
+                    self._funcs.append(funcInfo)              
+            case ast.Call():
+                self._opcalls.add(self.context.build_call(node))               
+            case ast.Import():
+                self._imports.update(alias.name for alias in node.names)                   
+            case ast.ImportFrom():
+                self._import_froms.extend(self.context.build_import_froms(node))                  
+            case ast.Assign() | ast.AnnAssign() | ast.AugAssign():
                 self._datacalls.update(self.context.build_datacalls(node, parent))
-
-            elif isinstance(node, ast.For):
+            case ast.For():
                 if isinstance(node.target, ast.Tuple):
                     for el in node.target.elts:
                         if not isinstance(el, ast.Constant):
                             self._iterator_vars.extend(self.context.build_iterator_var(el))
                 else:
                     self._iterator_vars.extend(self.context.build_iterator_var(node.target))
-            elif isinstance(node, ast.ListComp):
+            case ast.ListComp():
                 for iter in node.generators: 
                     self._iterator_vars.extend(self.context.build_iterator_var(iter.target))
                     
-            for child in ast.iter_child_nodes(node):
-                _walk_search(child, node)
-
-        _walk_search(self.context.file.get_ast(), None)
+        for child in ast.iter_child_nodes(node):
+            self._depth_first_search(child, node)
